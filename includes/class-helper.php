864001558666a0289652afb0dbeb52252810ae35
fabianpimminger <mail@fabianpimminger.com>
2021-02-14 12:09:08 +0100

    style: code formatting in helper.php

--- includes/class-helper.php
+++ includes/class-helper.php
@@ -10,21 +10,21 @@ namespace CookieFox;
 defined('ABSPATH') || exit;
 
 class Helper {
-	public static function get_option( $key = '', $default = false ) {
-    if ( function_exists( 'cmb2_get_option' ) ) {
-    	return cmb2_get_option( 'cookiefox', $key, $default );
-    }
-
-    $opts = get_option( 'cookiefox', $default );
-
-    $val = $default;
-
-    if ( 'all' == $key ) {
-      $val = $opts;
-    } elseif ( is_array( $opts ) && array_key_exists( $key, $opts ) && false !== $opts[ $key ] ) {
-      $val = $opts[ $key ];
-    }
-
-    return $val;
+	public static function get_option($key = '', $default = false) {
+		if (function_exists('cmb2_get_option')) {
+			return cmb2_get_option('cookiefox', $key, $default);
+		}
+	
+		$opts = get_option('cookiefox', $default);
+	
+		$val = $default;
+	
+		if ('all' == $key) {
+			$val = $opts;
+		} elseif (is_array($opts) && array_key_exists($key, $opts) && false !== $opts[ $key ]) {
+			$val = $opts[ $key ];
+		}
+	
+		return $val;
 	}
 }
