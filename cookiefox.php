0f417a00d669f466d1952d5727bcca2b7208f30d
fabianpimminger <mail@fabianpimminger.com>
2021-02-16 20:46:17 +0100

    chore: Requires at lease info updated

--- cookiefox.php
+++ cookiefox.php
@@ -3,6 +3,7 @@
  * Plugin Name: CookieFox
  * Description: A performant and accessible cookie and consent solution for WordPress.
  * Version: 1.0.5
+ * Requires at least: 5.0
  * Author: Fabian Pimminger
  * Author URI: https://fabianpimminger.com/work/
  * License: GPL2
